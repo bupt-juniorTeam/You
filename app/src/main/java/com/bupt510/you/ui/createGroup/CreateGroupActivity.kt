67508bf07b41a7d27f2e12687cd65de17186ba6d
alpha_Z <962279610@qq.com>
2021-03-26 21:02:59 +0800

    新建一个activity（虽然这个commit有点憨批，但前期谨慎一点）

---
+++ app/src/main/java/com/bupt510/you/ui/createGroup/CreateGroupActivity.kt
@@ -0,0 +1,12 @@
+package com.bupt510.you.ui.createGroup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bupt510.you.R
+
+class CreateGroupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_group)
+    }
+}
