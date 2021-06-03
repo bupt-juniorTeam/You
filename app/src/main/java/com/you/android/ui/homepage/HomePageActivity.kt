c90f3a5668d3bfcf31ac2b9d1545aa176b7820c3
pipixia626 <814151675@qq.com>
2021-06-03 17:01:26 +0800

    添加typeface textview # To-Do:用户中心菜单栏的UI美化、导航栏组件的合成

--- app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
+++ app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
@@ -2,11 +2,25 @@ package com.you.android.ui.homepage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.you.android.R
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        var toolbar=findViewById<Toolbar>(R.id.toolbar_person_center)
+
+        val drawer=findViewById<DrawerLayout>(R.id.drawerLayout_person_center)
+
+        var navigationView=findViewById<NavigationView>(R.id.navigationView_person_center)
+
+
+
+
     }
 }
