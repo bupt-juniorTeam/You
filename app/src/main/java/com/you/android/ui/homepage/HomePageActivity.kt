67ca6eac5cac8d3047bca805895d9de633cccb5c
Ideya Hiyona <hiyona.mocha@dralias.com>
2021-06-12 00:07:50 +0800

    将room list迁移到主界面

--- app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
+++ app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
@@ -1,34 +1,77 @@
 package com.you.android.ui.homepage
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.navigation.NavigationView
 import com.you.android.R
+import com.you.android.logic.model.RoomListResponse
 import com.you.android.ui.roomlist.RoomListActivity
-import kotlinx.coroutines.newFixedThreadPoolContext
+import com.you.android.ui.roomlist.RoomListAdapter
+import com.you.android.ui.roomlist.RoomListViewModel
+import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
-        var toolbar=findViewById<Toolbar>(R.id.toolbar_person_center)
+        val viewModel by lazy {
+            ViewModelProvider(this).get(RoomListViewModel::class.java)
+        }
 
-        val drawer=findViewById<DrawerLayout>(R.id.drawerLayout_person_center)
+        var toolbar = findViewById<Toolbar>(R.id.toolbar_person_center)
 
-        var navigationView=findViewById<NavigationView>(R.id.navigationView_person_center)
+        val drawer = findViewById<DrawerLayout>(R.id.drawerLayout_person_center)
 
-        var actionBarDrawerToggle :ActionBarDrawerToggle
+        var navigationView = findViewById<NavigationView>(R.id.navigationView_person_center)
+
+        var actionBarDrawerToggle: ActionBarDrawerToggle
 
         findViewById<Button>(R.id.searchRoomButton).setOnClickListener {
-            var intent:Intent= Intent(this,RoomListActivity::class.java)
+            val intent: Intent = Intent(this, RoomListActivity::class.java)
             startActivity(intent)
         }
 
+        val roomRecyclerView = findViewById<RecyclerView>(R.id.room_list)
+        roomRecyclerView.layoutManager = LinearLayoutManager(this@HomePageActivity)
+        val adapter = RoomListAdapter()
+        roomRecyclerView.adapter = AlphaInAnimationAdapter(adapter)
+
+        viewModel.roomsLiveData.observe(this, { result ->
+//            LogUtil.i(RoomListActivity.TAG, "获取聊天室")
+            val rooms = result.getOrNull()
+//            LogUtil.i(RoomListActivity.TAG, rooms.toString())
+            if (rooms != null) {
+                viewModel.roomList.clear()
+                viewModel.roomList.addAll(rooms)
+//                LogUtil.i(RoomListActivity.TAG, "聊天列表如下：")
+//                for (room in viewModel.roomList) {
+//                    LogUtil.i(RoomListActivity.TAG, room.name)
+//                }
+                adapter.room_list = rooms as ArrayList<RoomListResponse.Room>
+                lifecycleScope.launch(Dispatchers.Main) {
+                    adapter.notifyDataSetChanged()
+                }
+            } else {
+                Toast.makeText(this, "暂时没有聊天室", Toast.LENGTH_SHORT).show()
+                result.exceptionOrNull()?.printStackTrace()
+            }
+
+        })
+
+        //test
+        viewModel.searchRooms()
     }
 }
--- app/src/main/java/com/you/android/ui/roomlist/RoomListActivity.kt
+++ app/src/main/java/com/you/android/ui/roomlist/RoomListActivity.kt
@@ -3,7 +3,6 @@ package com.you.android.ui.roomlist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,9 +12,7 @@ import com.you.android.logic.model.RoomListResponse
 import com.you.android.util.LogUtil
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
-import okhttp3.Dispatcher
 
 class RoomListActivity : AppCompatActivity() {
     companion object {
@@ -28,22 +25,22 @@ class RoomListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_list)
-        val roomRecyclerView=findViewById<RecyclerView>(R.id.room_list)
+        setContentView(R.layout.room_list_activity)
+        val roomRecyclerView = findViewById<RecyclerView>(R.id.room_list)
         roomRecyclerView.layoutManager = LinearLayoutManager(this@RoomListActivity)
         val adapter = RoomListAdapter()
         roomRecyclerView.adapter = AlphaInAnimationAdapter(adapter)
 
         viewModel.roomsLiveData.observe(this, { result ->
-            LogUtil.i(TAG,"获取聊天室")
+            LogUtil.i(TAG, "获取聊天室")
             val rooms = result.getOrNull()
-            LogUtil.i(TAG,rooms.toString())
+            LogUtil.i(TAG, rooms.toString())
             if (rooms != null) {
                 viewModel.roomList.clear()
                 viewModel.roomList.addAll(rooms)
-                LogUtil.i(TAG,"聊天列表如下：")
-                for(room in viewModel.roomList) {
-                    LogUtil.i(TAG,room.name)
+                LogUtil.i(TAG, "聊天列表如下：")
+                for (room in viewModel.roomList) {
+                    LogUtil.i(TAG, room.name)
                 }
                 adapter.room_list = rooms as ArrayList<RoomListResponse.Room>
                 lifecycleScope.launch(Dispatchers.Main) {
