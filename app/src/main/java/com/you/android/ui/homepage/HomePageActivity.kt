a2696963e8eb20f4982d3b378e39ce981642057d
pipixia626 <814151675@qq.com>
2021-06-11 22:50:49 +0800

    添加 homepage UI TO-DO :修改Room list

--- app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
+++ app/src/main/java/com/you/android/ui/homepage/HomePageActivity.kt
@@ -1,12 +1,16 @@
 package com.you.android.ui.homepage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toolbar
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.you.android.R
+import com.you.android.ui.roomlist.RoomListActivity
+import kotlinx.coroutines.newFixedThreadPoolContext
 
 class HomePageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +23,12 @@ class HomePageActivity : AppCompatActivity() {
 
         var navigationView=findViewById<NavigationView>(R.id.navigationView_person_center)
 
+        var actionBarDrawerToggle :ActionBarDrawerToggle
 
-
+        findViewById<Button>(R.id.searchRoomButton).setOnClickListener {
+            var intent:Intent= Intent(this,RoomListActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
